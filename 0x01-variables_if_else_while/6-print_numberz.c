47f317bfe92586ef73b31398721b32e5c2505d98
Emmanuel Sonubi <Emmanuelsonubi@yahoo.com>
2023-03-17 21:32:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int dafe;
+for (dafe = 48; dafe < 58; dafe++)
+putchar(dafe);
+putchar('\n');
+return (0);
+}
+
